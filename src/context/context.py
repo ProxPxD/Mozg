99961c0233ce51b78f4082636990a1b04e09aae4
ProxPxD <piotr10tutek@poczta.onet.pl>
2026-08-08 23:05:54 +0200

    Ideas

--- src/context/context.py
+++ src/context/context.py
@@ -5,6 +5,7 @@ from typing import Self
 
 from context.defaults import Defaults
 from input import InputData
+from input.concepts import file
 
 
 @dataclass(kw_only=True)
@@ -12,3 +13,16 @@ class Context(Defaults):
     @classmethod
     def from_input_data(cls, input_data: InputData) -> Self:
         return cls(**asdict(input_data))
+
+'''
+Query Input States:
+ - From File
+ - From Editor
+ - From Query
+
+Keywords/Operations:
+ - Add     C
+ - Show    R
+ - Update  U
+ - Del     D
+'''
--- src/input/concepts/file.py
+++ src/input/concepts/file.py
@@ -1,4 +1,5 @@
 # TODO: replace in the future with sentinel('NO_FILE') and adjust type
 NO_FILE = object()
 EDITOR = object()
+FILE = object()
 type File = str | object
