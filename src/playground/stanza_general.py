0d63bd4c8890ef915aae6604b2212a29c4899d10
ProxPxD <piotr10tutek@poczta.onet.pl>
2026-08-18 11:42:23 +0200

    Rich playground analysis

--- src/playground/stanza_general.py
+++ src/playground/stanza_general.py
@@ -1,44 +1,85 @@
 import logging
+from pathlib import Path
 
 import pydash as _
 import stanza
 from pydash import chain as c
 from pydash import flow
-from stanza.models.common.doc import END_CHAR, START_CHAR, Document, Sentence, Word
+from stanza.models.common.doc import END_CHAR, FEATS, LEMMA, START_CHAR, Document, Sentence, Word
 from toolz import pipe
 
-LOGGING_LEVEL = logging.getLevelName(logging.WARNING)
+resources_path = Path(__file__).parent.parent.parent / 'resources'
 
-stanza.download(lang='en', logging_level=LOGGING_LEVEL)
-stanza.download(lang='pl', logging_level=LOGGING_LEVEL)
-
-nlp = stanza.Pipeline(
-    lang='en',
-    processors='tokenize,mwt,pos,lemma,depparse,ner',
-    logging_level=LOGGING_LEVEL,
-    use_gpu=True,
+resources_path.mkdir(exist_ok=True)
+settings = dict(
+    model_dir=str(resources_path),
+    # logging_level=logging.getLevelName(logging.WARNING),
 )
-doc: Document = nlp('John was needing to meet her at home. He needs it')
 
-words: list[Word] = list(doc.iter_words())
+logging.getLogger(stanza.__name__).setLevel(logging.WARNING)
 
-excluded_fields = {START_CHAR, END_CHAR}
+
+langs = ['en', 'pl']
+stanza.download(lang='multilingual', **settings)
+for lang in langs:
+    stanza.download(lang=lang, **settings)
+
+nlp = stanza.MultilingualPipeline(
+    download_method=None,
+    lang_id_config=dict(
+        langid_lang_subset=langs,
+    ),
+    lang_configs={
+        lang: dict(
+            processors='tokenize,mwt,pos,lemma,depparse,ner',
+            use_gpu=True,
+            **settings,
+        )
+        for lang in langs
+    },
+)
+
+raw_docs = [
+    '. '.join([
+        'John was needing to meet her at home',
+        'I know it',
+    ]),
+    '. '.join([
+        # 'Herbatę zrób',
+        'Będę mógł chcieć planować, żebyś spała',
+        'wiem to, że wiesz',
+        'da psu nożem jabłko',
+    ]),
+]
+docs: list[Document] = [Document([], text=raw_doc) for raw_doc in raw_docs]
+docs = nlp(docs)
+
+words: list[Word] = [word for doc in docs for word in doc.iter_words()]
+
+excluded_fields = {START_CHAR, END_CHAR, LEMMA, FEATS}
 fields = [field for field in words[0].to_dict() if field not in excluded_fields]
 maxes = {field: max(_.map_(words, flow(c().get(field), str, len))) for field in fields}
 
-for sent in doc.sentences:
-    print(f'{sent.id}:')
-    for ent in sent.ents:
-        print(f'{ent.text}: {ent.type}')
+for i, doc in enumerate(docs):
+    print(f'{i+1}) {doc.lang}')
+    for sent in doc.sentences:
+        print(f'{' '*2}{sent.index}) {sent.text}')
+        for ent in sent.ents:
+            print(f'{' '*4}{ent.text}: {ent.type}')
 
-for word in words:
-    word_dict = word.to_dict()
-    print(  # noqa: T201
-        *[
-            f'{field}: {word_dict.get(field, '-') :<{maxi}}' for field, maxi in maxes.items()
-        ],
-        sep=' ' * 4,
-    )
+for doc in docs:
+    print(f'{i+1}) {doc.lang}')
+    for sent in doc.sentences:
+        print(f'{' '*2}{sent.index}) {sent.text}')
+        for word in sent.words:
+            if word.upos == 'PUNCT':
+                continue
+            word_dict = word.to_dict()
+            print(' '*4, end='')
+            print(  # noqa: T201
+                *[f'{field}: {word_dict.get(field, "-"):<{maxi}}' for field, maxi in maxes.items()],
+                sep=' ' * 2,
+            )
 
 
 # print(
